ea865c4ceed679ff14e407486e7b0e52de81d7e5
sompoch <anawat@teohong.com>
2020-06-11 15:15:52 +0700

    customer

--- api/models/Customer.go
+++ api/models/Customer.go
@@ -1,5 +1,9 @@
 package models
+import (
+	"errors"
+	"github.com/jinzhu/gorm"
 
+)
 type Customer struct{
 	ID        uint32    `gorm:"primary_key;auto_increment" json:"id"`
 	CompanyName  string `gorm:"size:255;null;unique" json:"companyName"`
@@ -15,4 +19,54 @@ type Customer struct{
 	Mobile string `gorm:"size:100;null" json:"mobile"`
 	Fax string `gorm:"size:100;null" json:"fax"`
 	
+}
+
+func (c *Customer) Validate(action string) error {
+	if c.CompanyName==""{
+		return errors.New("Please input company name")
+	}
+	if c.ContactName==""{
+		return errors.New("Please input contact name")
+	}
+	if c.ContactTtle==""{
+		return errors.New("Please input contact title")
+	}
+	if c.Address==""{
+		return errors.New("Please input address")
+	}
+	if c.RegionID==0{
+		return errors.New("Please input region")
+	}
+	if c.ProvinceID == 0{
+		return errors.New("Please input province")
+	}
+	if c.DistrictID==0{
+		return errors.New("Please input district")
+	}
+	if c.SubDistrictID==0{
+		return errors.New("please input sub district")
+	}
+	if c.PostCode==""{
+		return errors.New("Please input postal code")
+	}
+	if c.Phone==""{
+		return errors.New("Please input phone")
+	}
+	if c.Mobile==""{
+		return errors.New("Please input mobile")
+	}
+	if c.Fax==""{
+		return errors.New("Please input fax")
+	}
+
+	return nil
+}
+func (c *Customer) SaveCustomer(db *gorm.DB) (*Customer, error) {
+
+	var err error
+	err = db.Debug().Create(&c).Error
+	if err != nil {
+		return &Customer{}, err
+	}
+	return c, nil
 }
